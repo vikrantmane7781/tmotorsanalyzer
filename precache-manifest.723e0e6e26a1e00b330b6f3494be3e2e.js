a1ef596e27ebe57a5f07efb85cee71cae0f47708
@nishantmane39 <nishantmane39@gmail.com>
2022-12-01 15:16:14 +0530

    Updates

---
+++ precache-manifest.723e0e6e26a1e00b330b6f3494be3e2e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "088e684988119da1ed0d",
+    "url": "/tmotorsanalyzer/static/css/main.34655ef0.chunk.css"
+  },
+  {
+    "revision": "088e684988119da1ed0d",
+    "url": "/tmotorsanalyzer/static/js/main.088e6849.chunk.js"
+  },
+  {
+    "revision": "d5fa0de6faffdf64f6d1",
+    "url": "/tmotorsanalyzer/static/js/1.d5fa0de6.chunk.js"
+  },
+  {
+    "revision": "a622a3211a3a852ffbef",
+    "url": "/tmotorsanalyzer/static/css/2.edc8352a.chunk.css"
+  },
+  {
+    "revision": "a622a3211a3a852ffbef",
+    "url": "/tmotorsanalyzer/static/js/2.a622a321.chunk.js"
+  },
+  {
+    "revision": "19cf934106d5ef09ad99",
+    "url": "/tmotorsanalyzer/static/js/runtime~main.19cf9341.js"
+  },
+  {
+    "revision": "229681fbf052d9b649a71143c30994c9",
+    "url": "/tmotorsanalyzer/static/media/cvrt2.229681fb.png"
+  },
+  {
+    "revision": "c94b762805d5092c5fbe1ec8f311bb65",
+    "url": "/tmotorsanalyzer/static/media/usericon.c94b7628.png"
+  },
+  {
+    "revision": "af97918b1b96082c97a0018fa8580f2d",
+    "url": "/tmotorsanalyzer/static/media/turbine.af97918b.png"
+  },
+  {
+    "revision": "d060882253730f86466320949524fa49",
+    "url": "/tmotorsanalyzer/static/media/headlight.d0608822.png"
+  },
+  {
+    "revision": "549fe54de0dd5b2bfae3001fd472a038",
+    "url": "/tmotorsanalyzer/static/media/bkg2.549fe54d.webp"
+  },
+  {
+    "revision": "198d0237dcbe81c9888c51432e6ec133",
+    "url": "/tmotorsanalyzer/index.html"
+  }
+];
